f04e37b1b88df95042837a505c2e22ecced5324f
Isabel B <isabelbolger@hotmail.com>
2020-01-23 16:54:28 -0500

    tentatively added createUser implementation client side

--- src/main/webapp/admin-page/user-admin.controller.client.js
+++ src/main/webapp/admin-page/user-admin.controller.client.js
@@ -1,19 +1,52 @@
 (function() {
     const $deleteBtn = $(".wbdv-tbody").find("#wbdv-remove");
-    var $usernameFld, $passwordFld;
-    var $removeBtn, $editBtn, $createBtn;
-    var $firstNameFld, $lastNameFld, $roleFld;
-    var $userRowTemplate, $tbody;
+    const $createBtn = $("#wbdv-create");
+    var $usernameFld = $('#usernameFld');
+    var $passwordFld = $('#passwordFld');
+    var $editBtn = $();
+    var $firstNameFld = $('#firstNameFld');
+    var $lastNameFld = $('#lastNameFld');
+    var $roleFld = $('.wbdv-role');
+    var $userRowTemplate = $('.wbdv-template');
+    var $tbody = $('.tasble');
     //var userService = new AdminUserServiceClient();
     $(main);
     
     $deleteBtn.click(deleteUser)
+    $createBtn.click(createUser)
     
     function main() {
 	 
     }
     
-    // function createUser() { … }
+     function createUser(event) {
+	 
+	 const username = $usernameFld.val()
+	 const password = $passwordFld.val()
+	 const firstname = $firstNameFld.val()
+	 const lastname = $lastNameFld.val()
+	 const role = $roleFld.val()
+	 console.log(username, password, firstname, lastname);
+	 
+	 const row = $userRowTemplate.clone();
+	 const usernameCol = row.find('.wbdv-username');
+	 const firstnameCol = row.find('.wbdv-first-name');
+	 const lastnameCol = row.find('.wbdv-last-name');
+	 const role1 = row.find('.wbdv-role');
+	 
+	 usernameCol.html(username);
+	 firstnameCol.html(firstname);
+	 lastnameCol.html(lastname);
+	 role1.html(role);
+	 
+	 $tbody.append(row);
+	 
+	 $usernameFld.val("");
+	 passwordFld.val("")
+	 firstNameFld.val("")
+	 lastNameFld.val("")
+	 
+     }
     // function findAllUsers() { … }
     // function findUserById() { … }
      function deleteUser(event) {
@@ -24,9 +57,9 @@
 	 
      }
     // function selectUser() { … }
-    // function updateUser() {
-    //	
-    // }
+     function updateUser() {
+    	
+     }
     // function renderUser(user) { … }
     // function renderUsers(users) { … }
      
