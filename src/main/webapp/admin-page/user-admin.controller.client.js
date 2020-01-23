501dbae8141a8cc5856839f7aa0e2c5d41c42a40
Isabel B <isabelbolger@hotmail.com>
2020-01-22 19:35:16 -0500

    user can be deleted statically

--- src/main/webapp/admin-page/user-admin.controller.client.js
+++ src/main/webapp/admin-page/user-admin.controller.client.js
@@ -1,18 +1,36 @@
-(function () {
+(function() {
+    const $deleteBtn = $(".wbdv-tbody").find("#wbdv-remove");
     var $usernameFld, $passwordFld;
     var $removeBtn, $editBtn, $createBtn;
     var $firstNameFld, $lastNameFld, $roleFld;
     var $userRowTemplate, $tbody;
-    var userService = new AdminUserServiceClient();
+    //var userService = new AdminUserServiceClient();
     $(main);
-
-    function main() { … }
-    function createUser() { … }
-    function findAllUsers() { … }
-    function findUserById() { … }
-    function deleteUser() { … }
-    function selectUser() { … }
-    function updateUser() { … }
-    function renderUser(user) { … }
-    function renderUsers(users) { … }
+    
+    $deleteBtn.click(deleteUser)
+    
+    function main() {
+	 
+    }
+    
+    // function createUser() { … }
+    // function findAllUsers() { … }
+    // function findUserById() { … }
+     function deleteUser(event) {
+	 console.log(event)
+	 currentTarget = $(event.currentTarget)
+	 const tr = currentTarget.parent().parent().parent();
+	 tr.hide();
+	 
+     }
+    // function selectUser() { … }
+    // function updateUser() {
+    //	
+    // }
+    // function renderUser(user) { … }
+    // function renderUsers(users) { … }
+     
+    
 })();
+
+
