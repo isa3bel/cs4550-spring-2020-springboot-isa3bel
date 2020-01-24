f3964cb0953df664d4f508ed65a98bb90d1ae795
Isabel B <isabelbolger@hotmail.com>
2020-01-23 21:07:36 -0500

    added user controller

---
+++ src/main/java/com/example/whiteboard/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.whiteboard.controller;
+
+import com.example.whiteboard.models.UserModel;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	static ArrayList<UserModel> users = new ArrayList<UserModel>();
+	static {
+		users.add(new UserModel(123, "alice", "Alice"));
+		users.add(new UserModel(234, "bob", "Bob"));
+		users.add(new UserModel(345, "charlie", "Charlie"));
+		users.add(new UserModel(456, "dan", "Dan"));
+	}
+	
+	@GetMapping("/users")
+	public ArrayList<UserModel> findAllUsers() {
+		return users;
+	}
+}
