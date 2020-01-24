8272485be6a08b3a1e3d1846e2312ff4f38008a6
Isabel B <isabelbolger@hotmail.com>
2020-01-23 21:04:46 -0500

    set up user server

--- src/main/webapp/admin-page/user.service.client.js
+++ src/main/webapp/admin-page/user.service.client.js
@@ -1,14 +1,21 @@
 function AdminUserServiceClient() {
-    this.createUser = createUser;
-    this.findAllUsers = findAllUsers;
-    this.findUserById = findUserById;
-    this.deleteUser = deleteUser;
-    this.updateUser = updateUser;
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/jannunzi/users';
-    var self = this;
-    function createUser(user) { … }
-    function findAllUsers() { … }
-    function findUserById(userId) { … }
-    function updateUser(userId, user) { … }
-    function deleteUser(userId) { … }
+    this.findAllUsers = findAllUsers
+  
+//    this.createUser = createUser;
+//    this.findAllUsers = findAllUsers;
+//    this.findUserById = findUserById;
+//    this.deleteUser = deleteUser;
+//    this.updateUser = updateUser;
+//    this.url = 'https://wbdv-generic-server.herokuapp.com/api/jannunzi/users';
+//    var self = this;
+//    function createUser(user) { … }
+       function findAllUsers() {
+    	return fetch('http://localhost:8080/users')
+        }
+	
+    
+    
+//    function findUserById(userId) { … }
+//    function updateUser(userId, user) { … }
+//    function deleteUser(userId) { … }
 }
